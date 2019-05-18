eabe11d020aadb5b2fbb2fbf7615a9cadab4fc8f
Manon Jacquin <manon.jacquin@gmail.com>
2019-05-18 19:38:04 +0100

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,56 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemons: [],
+    pokemonSearch: ''
+  }
+
+
+  getPokemons = () => {
+    fetch('http://localhost:3000/pokemon')
+      .then(resp => resp.json())
+      .then(pokemons => this.setState({pokemons}))
+  }
+
+  createPokemon = pokemon => {
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      header: {"Content-Type": "application/json"},
+      body: JSON.stringify(pokemon)
+    }).then(resp => resp.json())
+  }
+
+  updateState = (pokemon) => {
+    const pokemonsCopy = [...this.state.pokemons]
+    this.createPokemon(pokemon).then(data => {
+      pokemonsCopy.push(data)
+      this.setState({pokemons: pokemonsCopy})
+    })
+  }
+  
+  handleSearch = ({value}) => this.setState({pokemonSearch: value})
+  
+  componentDidMount() {
+    this.getPokemons()
+  }
+
   render() {
+    const showFilteredPokemon = this.state.pokemons.filter(poke => poke.name.includes(this.state.pokemonSearch))
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search 
+          onSearchChange={_.debounce(this.handleSearch, 500)} 
+          showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={showFilteredPokemon} />
         <br />
-        <PokemonForm />
+        <PokemonForm
+          createPokemon={this.updateState}
+        />
       </div>
     )
   }
